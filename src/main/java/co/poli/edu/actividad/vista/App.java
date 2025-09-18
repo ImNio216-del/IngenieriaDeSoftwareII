cbb061dd0621504fc2295d40984e683a589f636e
ImNio216-del <nikolasnino4@gmail.com>
2025-09-18 00:37:58 -0500

    Update App.java

--- src/main/java/co/poli/edu/actividad/vista/App.java
+++ src/main/java/co/poli/edu/actividad/vista/App.java
@@ -4,21 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    	AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/co/poli/edu/actividad/vista/formulario.fxml"));
+    	scene = new Scene(root);
         stage.setScene(scene);
+        stage.setTitle("Welcome");
         stage.show();
     }
 
